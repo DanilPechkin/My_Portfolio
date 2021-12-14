fe98fa82fecb37a6aff380deee9922784505525f
Danil <pppike15@gmail.com>
2021-12-14 23:25:17 +0500

    Now navMenu can switch fragments

--- app/src/main/java/com/example/myportfolio/MainActivity.kt
+++ app/src/main/java/com/example/myportfolio/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.myportfolio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
@@ -24,9 +25,12 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(toolbar)
 
+        navigationView.bringToFront()
         val toggle = ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
+        navigationView.setNavigationItemSelectedListener(this)
+
     }
 
     override fun onBackPressed() {
@@ -35,4 +39,28 @@ class MainActivity : AppCompatActivity() {
         else
             super.onBackPressed()
     }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        supportFragmentManager
+            .beginTransaction()
+            .addToBackStack(null)
+            .replace(R.id.nav_host_fragment, when(item.itemId){
+                R.id.nav_about -> AboutFragment()
+                R.id.nav_education -> EducationFragment()
+                R.id.nav_contacts -> ContactsFragment()
+                R.id.nav_plans -> PlansFragment()
+                R.id.nav_hobby -> HobbyFragment()
+                else -> {
+                    AboutFragment()
+                }
+            })
+            .commit()
+
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START)
+
+        return true
+    }
+
+
 }
